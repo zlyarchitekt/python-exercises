1ca1c334fae188ebaf156ef643fdb4e8fb62297f
Bartosz Wojciechowski <68919577+zlyarchitekt@users.noreply.github.com>
2020-08-11 23:23:29 +0200

    Update ex_001.py
    First test

--- ex_001.py
+++ ex_001.py
@@ -1,1 +1,21 @@
+import math
 
+def DecimalPi(userValue):
+    sp = round(math.pi, userValue)
+    # someList = list(p)
+    return sp
+
+# Border oof decimal places
+b = 20
+
+# User's input
+i = input("Please choose, how many decimal places PI should have: ")
+
+try:
+    r = int(i)
+    if r > b:
+        print("Please choose lower value.")
+    elif r <= b:
+        print(DecimalPi(r))
+except:
+    print("You didn't enter an integer")
