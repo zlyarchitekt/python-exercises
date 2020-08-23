c8294d47dbe110c8a1ab3533801fd0a985060034
Bartosz Wojciechowski <68919577+zlyarchitekt@users.noreply.github.com>
2020-08-23 23:58:33 +0200

    Ver02
    Correct except and limit

--- ex_001.py
+++ ex_001.py
@@ -1,20 +1,27 @@
+# Find PI to the Nth Digit
+# Enter a number and have the program
+# generate PI up to that many decimal places.
+# Keep a limit to how far the program will go.
+
 import math
+
+# Border oof decimal places
+b = 15
+
 
 def DecimalPi(userValue):
     sp = round(math.pi, userValue)
-    return sp
+    return print(f"Your result: {sp}")
 
-# Max value of decimal places
-b = 20
 
 # User's input
-i = input("Please choose, how many decimal places PI should have: ")
+i = input("Please choose how many decimal places PI should have: ")
 
 try:
     r = int(i)
     if r > b:
         print("Please choose lower value.")
     elif r <= b:
-        print(DecimalPi(r))
-except:
+        DecimalPi(r)
+except ValueError:
     print("You didn't enter an integer")
